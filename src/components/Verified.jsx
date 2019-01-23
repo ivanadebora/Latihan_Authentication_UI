52b51fe36d478cf46e70d44c82c91340f0f15b9a
ivanadebora <ivanadebora.wuwung@gmail.com>
2019-01-23 12:00:45 +0700

    commit baru

--- src/components/Verified.jsx
+++ src/components/Verified.jsx
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import queryString from 'query-string';
 
 class Verified extends Component {
     state = {verified: false, loading: true}
+
+    componentDidMount(){
+        var params = queryString.parse(this.props.location.search);
+        console.log(params);
+        var username = params.username;
+        var password = params.password;
+        axios.post('http://localhost:1212/auth/verified', {
+            username, password
+        })
+        .then((res) => {
+            const {token} = res.data;
+            localStorage.setItem("token", token || "");
+            this.props.onUserVerified(res.data);
+            console.log(res.data)
+            this.setState({loading:false, verified:true});
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+    
     renderContent = () => {
         if(this.state.verified && !this.loading){
             return(
